c29834918e275e766506cf1ec566f8d99da16d8a
Tomasuh <axel.em.olsson@gmail.com>
2016-03-01 14:48:02 +0100

    Start of work on installers

---
+++ monitoring/munin-node-installer.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+PKG_DIR="/tmp/munin-node/"
+
+PACKAGE="xxx.deb" #Should probably be an array where dependencies comes in order.
+
+if [[ $EUID -ne 0 ]]; then
+	echo "This installer must be run with root rights." 1>&2
+	exit 100
+fi
+
+dpkg -i "$PKG_DIR$PACKAGE"
+
+#Get package including deps list url for debs
+#apt-get --print-uris --yes install apache2 | grep ^\' | cut -d\' -f2
+#not working if package already installed.
