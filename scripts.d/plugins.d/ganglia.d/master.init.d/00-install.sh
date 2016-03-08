6e519115b60f50fe473acaccab382940e1b81741
Tomasuh <axel.em.olsson@gmail.com>
2016-03-08 11:31:40 +0100

    Restructuring of plugins

---
+++ scripts.d/plugins.d/ganglia.d/master.init.d/00-install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Get script directory
+DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "$DIR" ]]; then DIR="$PWD"; fi
+
+. "$DIR/../../script-utils/installer-utils.sh"
+. "$DIR/openmpi-package-list.sh"
+
+check_root
+
+apt-get install ganglia-monitor ganglia-monitor-python gmetad
+
+
