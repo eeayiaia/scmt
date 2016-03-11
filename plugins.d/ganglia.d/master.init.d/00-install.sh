03f30c6c2b2a7b7a47ad1992c4bad0530657fbb6
Tomasuh <axel.em.olsson@gmail.com>
2016-03-11 08:40:48 +0100

    Ganglia node installer and some work on master installer.

--- plugins.d/ganglia.d/master.init.d/00-install.sh
+++ plugins.d/ganglia.d/master.init.d/00-install.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+cluster_attr = "cluster{
+  name = ${2-my cluster}
+  owner = ${3-unspecified}
+  latlong = ${4-unspecified}
+  url = ${5-unspecified}
+}"
+
+udp_send_channel_attr = "udp_send_channel { 
+  host =  $(awk '/^[[:space:]]*($|#)/{next} /master/{print $1; exit}' /etc/hosts)
+  port = 8649
+  ttl = 1
+} "
+
+
 # Get script directory
 DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "$DIR" ]]; then DIR="$PWD"; fi
@@ -10,4 +24,11 @@ check_root
 
 apt-get install ganglia-monitor ganglia-monitor-python gmetad
 
+
+INSTALL_SUCCESS=$?
+
+if [[ $INSTALL_SUCCESS != 0 ]]; then
+    echo "Failed to install Ganglia." >&2
+    exit 1
+fi
 
