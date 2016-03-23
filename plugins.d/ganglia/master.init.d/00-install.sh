c51c6244c6b1371de55ba4610b73460539c69fb0
Tomasuh <axel.em.olsson@gmail.com>
2016-03-23 09:02:40 +0100

    Work on ganglia install script in unicast mode

--- plugins.d/ganglia/master.init.d/00-install.sh
+++ plugins.d/ganglia/master.init.d/00-install.sh
@@ -6,17 +6,49 @@ if [[ ! -d "$DIR" ]]; then DIR="$PWD"; fi
 
 . "$DIR/../../.script-utils/installer-utils.sh"
 
+send_metadata_interval=30
+
+globals_attr="
+  daemonize = yes
+  setuid = yes
+  user = nobody
+  debug_level = 0
+  max_udp_msg_len = 1472
+  mute = no
+  deaf = no
+  allow_extra_data = yes
+  host_dmax = 86400 /*secs. Expires (removes from web interface) hosts in 1 day$
+  host_tmax = 20 /*secs */
+  cleanup_threshold = 300 /*secs */
+  gexec = no
+  # By default gmond will use reverse DNS resolution when displaying your hostn$
+  # Uncommeting following value will override that value.
+  # override_hostname = \"mywebserver.domain.com\"
+  # If you are not using multicast this value should be set to something other $
+  # Otherwise if you restart aggregator gmond you will get empty graphs. 60 sec$
+  send_metadata_interval = $send_metadata_interval /*secs */
+"
+
 cluster_attr="
   name = ${1-my cluster}
   owner = ${2-unspecified}
   latlong = ${3-unspecified}
   url = ${4-unspecified}
 "
-
+#Assumes accessible them ip for eth0 is accessible for master.
+#Change to localhost possible?
 udp_send_channel_attr="
-  host =  $(awk '/^[[:space:]]*($|#)/{next} /master/{print $1; exit}' /etc/hosts)
+  host = localhost
   port = 8649
   ttl = 1
+"
+
+udp_recv_channel_attr="
+  port = 8649
+"
+
+tcp_accept_channel_attr="
+  port = 8649
 "
 
 data_source="data_source \"${1-my cluster}\" localhost\n"
@@ -33,8 +65,13 @@ if [[ $INSTALL_SUCCESS != 0 ]]; then
     exit 1
 fi
 
-python helpscript/regex.py "$cluster_attr" "$udp_send_channel_attr" "$data_source"
+python helpscript/regex.py "gmond" "globals" "$globals_attr"
+python helpscript/regex.py "gmond" "cluster" "$cluster_attr"
+python helpscript/regex.py "gmond" "udp_send_channel" "$udp_send_channel"
+python helpscript/regex.py "gmond" "udp_recv_channel" "$udp_recv_channel_attr"
+python helpscript/regex.py "gmond" "tcp_accept_channel" "$tcp_accept_channel_attr"
+python helpscript/regex.py "gmetad" "$data_source"
 
 ln -s -f /etc/ganglia-webfrontend/apache.conf /etc/apache2/conf-enabled/ganglia.conf
 
-service ganglia-monitor apache2 restart
+service ganglia-monitor apache2 gmetad restart
--- plugins.d/ganglia/master.init.d/helpscript/regex.py
+++ plugins.d/ganglia/master.init.d/helpscript/regex.py
@@ -13,27 +13,27 @@ def replace(sectionName, textToMatch, replaceWith):
             re.compile("(?<="+sectionName+" \{ )[^\}]*", re.MULTILINE), replaceWith, textToMatch
             )
 
-if len(sys.argv) != 4:
-  sys.stderr.write("Param 1: content for cluster{ } section gmond.conf.\n" \
-                   "Param 2: content for udp_send_channel{ } section gmond.conf.\n" \
-                   "Param 3: content for data_source line in gmetad.conf.\n")
-  exit(1)
+if len(sys.argv)==4 and sys.argv[1]=="gmond":
+  with open('/etc/ganglia/gmond.conf', 'r+') as f:
+    print "Editing gmond.conf..."
+    data=f.read()
+    f.seek(0)
+    data = replace(sys.argv[2], data, sys.argv[3])
+    f.write(data)
+    f.truncate()
+    f.close()
 
-with open('/etc/ganglia/gmond.conf', 'r+') as f:
-  print "Editing gmond.conf..."
-  data=f.read()
-  f.seek(0)
-  data = replace("cluster", data, sys.argv[1])
-  data = replace("udp\_send\_channel", data, sys.argv[2])
-  f.write(data)
-  f.truncate()
-  f.close()
+elif len(sys.argv)==3 and sys.argv[1]=="gmetad":
+  with open('/etc/ganglia/gmetad.conf', 'r+') as f:
+    print "Editing gmetad.conf..."
+    data=f.read()
+    f.seek(0)
+    data = re.sub("data\_source \"my cluster\" localhost\n", sys.argv[2], data)
+    f.write(data)
+    f.truncate()
+    f.close()
 
-with open('/etc/ganglia/gmetad.conf', 'r+') as f:
-  print "Editing gmetad.conf..."
-  data=f.read()
-  f.seek(0)
-  data = re.sub("data\_source \"my cluster\" localhost\n", sys.argv[3], data)
-  f.write(data)
-  f.truncate()
-  f.close()
+else:
+  print sys.argv[0] + " gmond section content"
+  print sys.argv[0] + " gmetad data_source...."
+  exit(0)
