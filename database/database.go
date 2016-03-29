a0133e946c308b3c9b003576a75d17e7453744a7
sebastian <me@sebastianlund.se>
2016-03-29 07:58:28 +0200

    refactor database/database.go to follow standard

--- database/database.go
+++ database/database.go
@@ -2,19 +2,19 @@ package database
 
 import (
 	"database/sql"
+	log "github.com/Sirupsen/logrus"
 	_ "github.com/go-sql-driver/mysql"
 	"strings"
-    log "github.com/Sirupsen/logrus"
-    "sync"
+	"sync"
 )
 
 type Slave struct {
 	Hostname        string
 	HardwareAddress string
 	IpAddress       string
-	Port	string
-	UserName string
-	Password string
+	Port            string
+	UserName        string
+	Password        string
 
 	lock *sync.Mutex // Lock before changing ..
 
@@ -22,64 +22,63 @@ type Slave struct {
 	Connected     bool
 }
 
-
 //User:password@/database
-func connectDB() *sql.DB{
+func connectDB() *sql.DB {
 	db, err := sql.Open("mysql", "master:badpassword@/cluster")
-	if (err != nil){
+	if err != nil {
 		Log.WithFields(log.Fields{
 			"error": err,
 		}).Fatal("Could not connect to database")
 		return nil
-	}else{
+	} else {
 		return db
 	}
-	
+
 }
 
 func AddDevice(device Slave) {
 	db := connectDB()
-	if (db == nil){
+	if db == nil {
 		return
 	}
 	defer db.Close()
 	//INET_ATON is to convert ip to int (array-TO-number)
 	stmt, err := db.Prepare("INSERT devices SET hwaddr=?, ip=INET_ATON(?), port=?, hname=?, username=?, password=?")
-	if(err != nil){
+	if err != nil {
 		Log.WithFields(log.Fields{
 			"error": err,
 		}).Fatal("Could not prepare sql statement")
 		return
 	}
 
-	_, err = stmt.Exec(strings.Replace(device.HardwareAddress , ":", "", -1), device.IpAddress, device.Port, device.Hostname, device.UserName, device.Password)
-	if(err != nil){
+	_, err = stmt.Exec(strings.Replace(device.HardwareAddress, ":", "", -1), device.IpAddress, device.Port, device.Hostname, device.UserName, device.Password)
+	if err != nil {
 		Log.WithFields(log.Fields{
 			"error": err,
 		}).Fatal("Could not execute the sql query")
 		return
-	}	
+	}
 	Log.WithFields(log.Fields{
 		"mac": device.HardwareAddress,
 	}).Info("added device")
-	
+
 }
 
 func DeleteDevice(HWAddr string) {
 	db := connectDB()
-	if(db == nil){
+	if db == nil {
 		return
 	}
 	defer db.Close()
 	stmt, err := db.Prepare("DELETE FROM devices WHERE hwaddr=?")
-	if(err != nil){
+	if err != nil {
 		Log.WithFields(log.Fields{
 			"error": err,
 		}).Fatal("Could not prepare sql statement")
 		return
 	}
 	device, err := stmt.Exec(strings.Replace(HWAddr, ":", "", -1))
-	if(err != nil){
+	if err != nil {
 		Log.WithFields(log.Fields{
 			"error": err,
 		}).Fatal("Could not execute the sql query")
@@ -87,7 +86,7 @@ func DeleteDevice(HWAddr string) {
 	}
 
 	affect, err := device.RowsAffected()
-	if(err != nil){
+	if err != nil {
 		Log.WithFields(log.Fields{
 			"error": err,
 		}).Fatal("Could not calculate number of affected rows")
@@ -106,13 +105,13 @@ func DeleteDevice(HWAddr string) {
 
 func GetDevice(HWaddr string) *Slave {
 	db := connectDB()
-	if(db == nil){
+	if db == nil {
 		return nil
 	}
 	defer db.Close()
 
 	stmt, err := db.Prepare("SELECT HWaddr, hname, INET_NTOA(ip), port, username, password FROM devices WHERE HWaddr=?")
-	if(err != nil){
+	if err != nil {
 		Log.WithFields(log.Fields{
 			"error": err,
 		}).Fatal("Could not prepare sql statement")
@@ -140,33 +139,32 @@ func GetDevice(HWaddr string) *Slave {
 	}
 }
 
-func GetAllDevices() []*Slave{
-    db := connectDB()
-	if(db == nil){
+func GetAllDevices() []*Slave {
+	db := connectDB()
+	if db == nil {
 		return nil
 	}
 	defer db.Close()
-    
-    rows, err := db.Query("SELECT HWaddr, hname, INET_NTOA(ip), port, username, password FROM devices")
-    if(err != nil){
-    	Log.WithFields(log.Fields{
-    		"error": err,
-    	}).Fatal("Could not execute sql query")
-    	return nil
-    }
-    defer rows.Close()
-    var ds []*Slave
-    for rows.Next(){
-        device := &Slave{}
-        rows.Scan(&device.HardwareAddress, &device.Hostname, &device.IpAddress , &device.Port, &device.UserName, &device.Password)
-        ds = append(ds, device)
-    }
-   	Log.Info("Returned all devices in database")
-    return ds
+
+	rows, err := db.Query("SELECT HWaddr, hname, INET_NTOA(ip), port, username, password FROM devices")
+	if err != nil {
+		Log.WithFields(log.Fields{
+			"error": err,
+		}).Fatal("Could not execute sql query")
+		return nil
+	}
+	defer rows.Close()
+	var ds []*Slave
+	for rows.Next() {
+		device := &Slave{}
+		rows.Scan(&device.HardwareAddress, &device.Hostname, &device.IpAddress, &device.Port, &device.UserName, &device.Password)
+		ds = append(ds, device)
+	}
+	Log.Info("Returned all devices in database")
+	return ds
 }
 
 //Inserts colons in hardware adddevices for readability
 func insertColon(s string) string {
 	return strings.Join([]string{s[0:2], s[2:4], s[4:6], s[6:8], s[8:10], s[10:12]}, ":")
 }
-	
