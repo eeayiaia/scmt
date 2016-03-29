7226f15dd07579605f827a66f43b165a86159c74
sebastian <me@sebastianlund.se>
2016-03-29 15:17:36 +0200

    use syslog in production as well

--- logger.go
+++ logger.go
@@ -3,11 +3,24 @@ package main
 import (
 	log "github.com/Sirupsen/logrus"
 	prefixed "github.com/x-cray/logrus-prefixed-formatter"
+
+	logrus_syslog "github.com/Sirupsen/logrus/hooks/syslog"
+	"log/syslog"
 )
 
 func InitLogging() {
 	// Default to the prefixed formatter
 	log.SetFormatter(new(prefixed.TextFormatter))
+
+	// Add syslog as secondary logging (only if in production)
+	if Conf.Production {
+		hook, err := logrus_syslog.NewSyslogHook("udp", "localhost:514", syslog.LOG_INFO, "")
+		if err != nil {
+			log.Error("Unable to connect to local syslog daemon")
+		} else {
+			log.AddHook(hook)
+		}
+	}
 
 	log.Info("Initialised logging")
 }
