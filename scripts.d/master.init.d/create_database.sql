0b57b22140daf0136f723c6f101dd67ebb2f9bf1
Tomasuh <axel.em.olsson@gmail.com>
2016-04-04 10:36:15 +0200

    Added a simple plugin structure for database

--- scripts.d/master.init.d/create_database.sql
+++ scripts.d/master.init.d/create_database.sql
@@ -1,5 +1,13 @@
 CREATE DATABASE IF NOT EXISTS cluster;
 USE cluster;
 CREATE TABLE IF NOT EXISTS devices (hwaddr CHAR(12) KEY, ip INT UNSIGNED NOT NULL, port INT UNSIGNED, hname varchar(30) NOT NULL, username VARCHAR(20) NOT NULL, password VARCHAR(20)NOT NULL);
+CREATE TABLE IF NOT EXISTS installedOn (
+    hwaddr CHAR(12) NOT NULL,
+    plugin VARCHAR(30) NOT NULL,
+    FOREIGN KEY (hwaddr) REFERENCES devices(hwaddr),
+    FOREIGN KEY (plugin) REFERENCES plugins(name),
+    UNIQUE (hwaddr, plugin)
+);
+
 GRANT ALL PRIVILEGES ON cluster . * TO master@localhost IDENTIFIED BY 'badpassword';
 FLUSH PRIVILEGES;
