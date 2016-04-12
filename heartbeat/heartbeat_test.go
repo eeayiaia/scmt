284118341e3f2f3f17429cb3ffd81c8aec25cce2
sebastian <me@sebastianlund.se>
2016-04-12 14:32:32 +0200

    remove ping-tests because lacking support in travis

--- heartbeat/heartbeat_test.go
+++ heartbeat/heartbeat_test.go
@@ -6,47 +6,6 @@ import (
 	"time"
 )
 
-// Test the Ping(address) function!
-func PingTest(t *testing.T) {
-	if testing.Short() {
-		t.Skip("skipping in short mode, failure takes a while.")
-	}
-
-	if !Ping("127.0.0.1") {
-		t.Error("Could not ping localhost!")
-	}
-
-	if Ping("127.1.1.1") {
-		t.Error("could ping a non-existent address (127.1.1.1)!")
-	}
-}
-
-// Test that the pinger works correctly
-func TestPinger(t *testing.T) {
-	if testing.Short() {
-		t.Skip("skipping in short mode, this takes a while.")
-	}
-
-	disconnectCh := make(chan bool, 1)
-
-	timeout := make(chan bool, 1)
-	go func() {
-		time.Sleep(1 * time.Second)
-		timeout <- true
-	}()
-
-	ch := Pinger("127.0.0.1", func(_ string) {
-		disconnectCh <- false
-	})
-	select {
-	case <-disconnectCh:
-		t.Error("could not ping ourselves!")
-	case <-timeout:
-		// Moving on!
-		ch <- false
-	}
-}
-
 // Test filterAddress function to properly filter the address
 func FilterAddressTest(t *testing.T) {
 	addr := "127.0.0.1:1234"
