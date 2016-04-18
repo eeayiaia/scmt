2b69b6b5b25d0e38dbe30777f1a24ef153a5b23e
sebastian <me@sebastianlund.se>
2016-04-18 09:33:09 +0200

    fix the cli tests

--- cli/commands_test.go
+++ cli/commands_test.go
@@ -1,31 +1,33 @@
 package cli
 
 import (
-	"testing"
+	"flag"
 	"fmt"
 	"github.com/codegangsta/cli"
-	"flag"
+	"testing"
 )
 
-func TestGetCommands (t *testing.T) {
+func TestGetCommands(t *testing.T) {
 	command := getCommands()
 	length := len(command)
 
-	if len(command) != 3 {
+	if len(command) != len(commands) {
 		t.Errorf("Length of command list is not 3 it is %s", length)
 	}
 }
 
-func TestAddCommand (t *testing.T) {
+func TestAddCommand(t *testing.T) {
+	formerLen := len(getCommands())
+
 	AddCommand("test", []string{""}, "", "", "", "", func(c *cli.Context) { fmt.Println("the test action method") })
 
 	command := getCommands()
-	if len(command) != 4 {
+	if len(command) != formerLen+1 {
 		t.Errorf("Add command has not added a command!")
 	}
 
 	context := cli.NewContext(cli.NewApp(), flag.NewFlagSet("testflag", 1), nil)
-	commandToRun := command[3]
+	commandToRun := command[len(commands)-1]
 
 	err := commandToRun.Run(context)
 
