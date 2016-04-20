98fde45634c1db7c60741ab75798931ea41d02c0
Elvira Jonsson <elviraj@student.chalmers.se>
2016-04-20 14:48:09 +0200

    Added device_handler to database package

---
+++ database/device_handler.go
@@ -0,0 +1,10 @@
+package database
+//Accessing information about devices
+
+func AllDeviceStatus() {
+	//This is the function to be run when a user wants to list node status..
+	/*db, _ := NewConnection()
+	defer db.Close()
+
+	rows, err := db.Query("SELECT * FROM devices")*/
+}
