77393e2ff7da2a9aa5c2a2ad58ee76f9ad415baa
Magnus Bergsten <magbergs@student.chalmers.se>
2016-04-20 14:44:21 +0200

    Fix nfs setup script

--- scripts.d/device.init.d/40-setup-nfs-client.sh
+++ scripts.d/device.init.d/40-setup-nfs-client.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
+# Get script directory & include utils
+DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "$DIR" ]]; then DIR="$PWD"; fi
+
+. "$DIR/../utils.sh" || exit 1
+
+check_invoked_by_scmt
 
 #Install nfs
-sudo apt-get install portmap nfs-common
+apt-get install portmap nfs-common --assume-yes
 INSTALL_SUCCESS=$?
 
 echo ""
 write_line
 
 if [[ $INSTALL_SUCCESS != 0 ]]; then
-		echo "Failed to install NFS on node."
-		exit 1
+	echo "Failed to install NFS on node."
+	exit 1
 fi
 
 #Adding rpcbind
@@ -20,15 +27,14 @@ echo "rpcbind : ALL" >> /etc/hosts.deny
 echo "10.46.0.1" >> /etc/hosts.allow
 
 #Create mounted shared folder
-sudo mkdir /var/shared
+if [[ ! -d /var/shared ]]; then
+	mkdir /var/shared
+fi
 
 #Mount the server shared folder to the local folder
-sudo mount 10.46.0.1:/var/nfs /var/shared/
-
+mount 10.46.0.1:/var/nfs /var/shared/
 
 #Start services
-sudo /etc/init.d/portmap restart
-sudo /etc/init.d/nfs-common restart
-
-
+/etc/init.d/portmap restart
+/etc/init.d/nfs-common restart
 
