bc677a1b5be87237637dac576233b99679d79094
Magnus Bergsten <magbergs@student.chalmers.se>
2016-04-25 16:13:35 +0200

    Added logging and file backup to device.init scripts

--- scripts.d/device.init.d/00-set-hostname.sh
+++ scripts.d/device.init.d/00-set-hostname.sh
@@ -1,9 +1,17 @@
-#!/bin/sh
+#!/bin/bash
 
 # Supplied environment variables:
 #   HOSTNAME
-#   NODE_IP
-#   NODE_MAC
+
+# Get script directory & include utils
+DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "$DIR" ]]; then DIR="$PWD"; fi
+
+. "$DIR/../utils.sh" || exit 1
+
+check_invoked_by_scmt
 
 # Set correct hostname
+backup_file /etc/hostname
 echo $HOSTNAME > /etc/hostname
+
--- scripts.d/device.init.d/10-set-hosts.sh
+++ scripts.d/device.init.d/10-set-hosts.sh
@@ -1,7 +1,17 @@
-#!/bin/sh
+#!/bin/bash
 
 # Input: MASTER_IP
 # Set the hosts file to include the master node
+
+# Get script directory & include utils
+DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "$DIR" ]]; then DIR="$PWD"; fi
+
+. "$DIR/../utils.sh" || exit 1
+
+check_invoked_by_scmt
+
+backup_file /etc/hosts
 
 egrep -q "^master\s" /etc/hosts \
 	&& sed "s/master/$MASTER_IP    master/" -i /etc/hosts \
