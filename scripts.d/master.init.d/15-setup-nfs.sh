f2194d538afab75ac1ad4f38744300285feb7b3c
Magnus Bergsten <magbergs@student.chalmers.se>
2016-04-28 16:15:26 +0200

    Fix relative paths in NFS setup

--- scripts.d/master.init.d/15-setup-nfs.sh
+++ scripts.d/master.init.d/15-setup-nfs.sh
@@ -36,10 +36,20 @@ backup_file "$EXPORTS"
 echo "/var/nfs	$CLUSTER_SUBNET(rw,sync,no_subtree_check)" >> "$EXPORTS"
 
 # Link the correct directories into nfs
-ln -sf "$DIR/../../scripts.d" "/var/nfs/scripts.d"
-ln -sf "$DIR/../../plugins.d" "/var/nfs/plugins.d"
-ln -sf "$DIR/../../configs" "/var/nfs/configs" 
-ln -sf "$DIR/../utils.sh" "/var/nfs/utils.sh"
+SCRIPTS_PATH=$(realpath '$DIR/../../scripts.d')
+PLUGINS_PATH=$(realpath '$DIR/../../plugins.d')
+CONFIGS_PATH=$(realpath '$DIR/../../configs')
+UTILS_PATH=$(realpath '$DIR/../utils.sh')
+
+[[ -f "$SCRIPTS_PATH" ]] && delete_file "$SCRIPTS_PATH"
+[[ -f "$PLUGINS_PATH" ]] && delete_file "$PLUGINS_PATH"
+[[ -f "$CONFIGS_PATH" ]] && delete_file "$CONFIGS_PATH"
+[[ -f "$UTILS_PATH" ]] && delete_file "$UTILS_PATH"
+
+ln -sf "$SCRIPTS_PATH" "/var/nfs/scripts.d"
+ln -sf "$PLUGINS_PATH" "/var/nfs/plugins.d"
+ln -sf "$CONFIGS_PATH" "/var/nfs/configs"
+ln -sf "$UTILS_PATH" "/var/nfs/utils.sh"
 
 # Create the nfs table
 exportfs -a
