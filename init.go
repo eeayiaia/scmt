e42a9efdd269b7b79f3526d42a8f7bfccd992de6
Elvira Jonsson <elviraj@student.chalmers.se>
2016-04-29 14:21:23 +0200

    Created method stubs for first setup

---
+++ init.go
@@ -0,0 +1,115 @@
+package main 
+
+import (
+	//log "github.com/Sirupsen/logrus"
+	"github.com/eeayiaia/scmt/conf"
+	"fmt"
+	"bufio"
+	"os"
+	"strings"
+)
+
+var creds []*conf.Credentials = []*conf.Credentials{
+   	&conf.Credentials{Username: "odroid",
+   	Password:  "odroid"},
+}
+var config conf.Configuration = conf.Configuration{
+   	Production: false,
+   	ClusterName: "scmt",
+   	ClusterSubnet: "10.46.0.0/24",
+   	ClusterBroadcastIP: "10.46.0.255",
+   	DeviceIPRangeBegin: "10.46.0.10",
+   	DeviceIPRangeEnd: "10.46.0.200",
+    MasterIP: "10.46.0.1",
+    DHCPDLeaseTimeDefault: "86400",
+    DHCPDLeaseTimeMax: "172800",
+    InvokedBySCMT: "1",
+    Database: "cluster",
+    DatabaseUser: "master",
+    DatabasePassword: "badpassword",
+    LoginCredentials: creds,
+    PidFile: "scmt.pid",
+    LogFile: "scmt.log",
+}
+
+var reader *bufio.Reader = bufio.NewReader(os.Stdin) 
+
+var monitorName string = "none"
+var clusterAppName string = "none"
+
+//TODO: check if scmt.json exists, here or in main
+func FirstSetup() {
+	fmt.Println("Welcome to SCMT setup wizard! We start by setting up the configuration: (Exit by entering 'q')")
+	setClusterName()
+	setClusterSubnet()
+	setBroadcastIP()
+	setDeviceIPRange()
+	setMasterIP()
+	setDatabaseName()
+	setDatabaseUser()
+	setDatabasePw()
+	setLoginCred()
+}
+
+func quit(ans string) bool {
+	return strings.Compare("q", strings.ToLower(strings.TrimSpace(ans))) == 0
+}
+
+func setClusterName() {
+	fmt.Println("The default cluster name is '" + config.ClusterName + "' type new name to change or press enter to keep default name")
+	ans, _ := reader.ReadString('\n')
+	switch ans {
+	case "\n":
+		return
+	case "q":
+		fmt.Println("Terminating..")
+		os.Exit(0)
+	default:
+		config.ClusterName = ans
+		return
+	}
+}
+
+func setClusterSubnet() {
+	
+}
+
+func setBroadcastIP() {
+	
+}
+
+func setDeviceIPRange() {
+	
+}
+
+func setMasterIP() {
+	
+}
+
+func setDatabaseName() {
+	
+}
+
+func setDatabaseUser() {
+	
+}
+
+func setDatabasePw() {
+	
+}
+
+func setLoginCred() {
+
+}
+
+func monitor() {
+
+}
+
+func clusterApp() {
+
+}
+
+func setup() {
+
+}
