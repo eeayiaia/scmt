9ef249ed8a1bdb30e27ea11e6f575c255e98a2a0
Axel olsson <axel.em.olsson@gmail.com>
2016-05-03 13:42:36 +0200

    Bug fix munin device init script

--- plugins.d/munin/device.init.d/00-install.sh
+++ plugins.d/munin/device.init.d/00-install.sh
@@ -3,8 +3,6 @@
 # Include utils
 . "/var/shared/utils.sh" || exit 1
 
-master_IP=$(awk '/^[[:space:]]*($|#)/{next} /master/{print $1; exit}' /etc/hosts)
-
 if [[ $EUID -ne 0 ]]; then
 	echo "This installer must be run with root rights." 1>&2
 	exit 100
@@ -16,6 +14,6 @@ apt-get install -y --force-yes munin-node
 #Backup config file
 backup_file /etc/munin/munin-node.conf
 
-echo "cidr_allow $master_IP/32" >> /etc/munin/munin-node.conf
+echo "cidr_allow $MASTER_IP/32" >> /etc/munin/munin-node.conf
 
 service munin-node restart
