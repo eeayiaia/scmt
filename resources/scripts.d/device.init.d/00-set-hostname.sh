a14410bcf59a7544586bb8342156fa11210f7d67
Magnus Bergsten <magbergs@student.chalmers.se>
2016-05-10 17:40:18 +0200

    Fix and robustness for devicehostname and hosts setter scripts

--- resources/scripts.d/device.init.d/00-set-hostname.sh
+++ resources/scripts.d/device.init.d/00-set-hostname.sh
@@ -9,7 +9,21 @@ check_invoked_by_scmt
 
 backup_file "/etc/hostname"
 
+assertIsSet NODE_HOSTNAME
+
+# Find old hostname
+OLDNAME=$(cat /etc/hostname)
+
 # Set correct hostname
 echo $NODE_HOSTNAME >| /etc/hostname
 echo "Set hostname to $NODE_HOSTNAME"
+
+# Update hostfile to include new hostname
+if [[ $OLDNAME == "" ]]; then
+	# This should of course not happen, but just to be sure...
+	echo "127.0.0.1	${NODE_HOSTNAME}" >> /etc/hosts
+else
+	echo "Updating hostname in /etc/hosts"
+	sed "s/.*${OLDNAME}/127.0.0.1    ${NODE_HOSTNAME}/" -i /etc/hosts
+fi
 
--- resources/scripts.d/device.init.d/10-set-hosts.sh
+++ resources/scripts.d/device.init.d/10-set-hosts.sh
@@ -3,14 +3,23 @@
 # Input: MASTER_IP
 # Set the hosts file to include the master node
 
-. "$UTILS_PATH"
+. "$UTILS_PATH" || exit 1
 
 check_invoked_by_scmt
 
+assertIsSet MASTER_IP
+
 backup_file /etc/hosts
 
-egrep -q "^master\s" /etc/hosts \
-	&& sed "s/master/${MASTER_IP}    master/" -i /etc/hosts \
-	|| sed "$ a\
+egrep -q "^\s*\S+\s*master\s*$" /etc/hosts
+UPDATE=$?
+
+if [[ $UPDATE == 0 ]]; then
+	echo "Updating master address in /etc/hosts"
+	sed "s/.*\smaster/${MASTER_IP}    master/" -i /etc/hosts
+else
+	echo "Adding master to /etc/hosts"
+	sed "$ a\
 ${MASTER_IP}    master" -i /etc/hosts
+fi
 
