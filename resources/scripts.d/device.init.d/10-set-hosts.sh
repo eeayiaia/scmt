f521d96a18f84f1ebe201c6579f9109d94e67ef1
Magnus Bergsten <magbergs@student.chalmers.se>
2016-05-10 14:36:06 +0200

    10-set-hosts now uses UTILS_PATH (fixed bugs too)

--- resources/scripts.d/device.init.d/10-set-hosts.sh
+++ resources/scripts.d/device.init.d/10-set-hosts.sh
@@ -1,24 +1,13 @@
-i#!/bin/bash
+#!/bin/bash
 
 # Input: MASTER_IP
 # Set the hosts file to include the master node
 
-if [[ ! $INVOKED_BY_SCMT ]]; then
-	echo "This script is intended to be invoked by SCMT, not manually." >&2
-	exit 1
-fi
+. "$UTILS_PATH"
 
-BACKUP_FOLDER=~/.scmt-backup
-DATE_STAMP=$(date "+%b_%d_%Y_%H:%M:%S")
-BACKUP_FILE=/etc/hostname
-BACKUP_OUTPUT=$BACKUP_FOLDER/$BACKUP_FILE-$DATE_STAMP
+check_invoked_by_scmt
 
-if [[ ! -d "$BACKUP_FOLDER" ]]; then
-	mkdir "$BACKUP_FOLDER"
-fi
-
-echo "Backing up file $BACKUP_FILE to $BACKUP_OUTPUT..."
-cp "$BACKUP_FILE" "$BACKUP_OUTPUT"
+backup_file /etc/hosts
 
 egrep -q "^master\s" /etc/hosts \
 	&& sed "s/master/${MASTER_IP}    master/" -i /etc/hosts \
