1d58faf5b38752c6430ea13b5afd8706ca01f4e8
Magnus Bergsten <magbergs@student.chalmers.se>
2016-05-10 14:25:39 +0200

    Testint using $UTILS_PATH in 00-set-hostname.sh

--- resources/scripts.d/device.init.d/00-set-hostname.sh
+++ resources/scripts.d/device.init.d/00-set-hostname.sh
@@ -1,21 +1,13 @@
 #!/bin/bash
 
 # Supplied environment variables:
-#   HOSTNAME
+#   NODE_HOSTNAME
 
-[[ $INVOKED_BY_SCMT == 1 ]] || exit 1
+. "$UTILS_PATH"
 
-BACKUP_FOLDER=~/.scmt-backup
-DATE_STAMP=$(date "+%b_%d_%Y_%H:%M:%S")
-BACKUP_FILE=/etc/hostname
-BACKUP_OUTPUT=$BACKUP_FOLDER/$BACKUP_FILE-$DATE_STAMP
+check_invoked_by_scmt
 
-if [[ ! -d "$BACKUP_FOLDER" ]]; then
-	mkdir "$BACKUP_FOLDER"
-fi
-
-echo "Backing up file $BACKUP_FILE to $BACKUP_OUTPUT..."
-cp "$BACKUP_FILE" "$BACKUP_OUTPUT"
+backup_file "/etc/hostname"
 
 # Set correct hostname
 echo $NODE_HOSTNAME > /etc/hostname
